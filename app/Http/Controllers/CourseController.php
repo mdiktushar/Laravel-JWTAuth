1e6215c616a689264b436e8f1750355c94069f72
mdiktushar <md.tushar1804@gmail.com>
2023-09-28 14:00:57 +0600

    controller setup

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -7,4 +7,15 @@ use Illuminate\Http\Request;
 class CourseController extends Controller
 {
     //
+    public function courseEnrollment(Request $request) {
+
+    }
+
+    public function totalCourses() {
+
+    }
+
+    public function deleteCourse() {
+        
+    }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,4 +7,20 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     //
+
+    public function register(Request $request) {
+
+    }
+
+    public function login(Request $request) {
+
+    }
+
+    public function profile() {
+
+    }
+
+    public function logout() {
+
+    }
 }
