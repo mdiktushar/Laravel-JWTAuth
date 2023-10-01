06fbcf8b7e351fd39095bfb340cc37acd97c01f3
mdiktushar <md.tushar1804@gmail.com>
2023-10-01 12:46:54 +0600

    register

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -9,7 +10,24 @@ class UserController extends Controller
     //
 
     public function register(Request $request) {
+        $request->validate([
+            'name' => "required",
+            'email' => "required|email|unique:users",
+            'password' => "required|confirmed"
+        ]);
 
+        $user = new User();
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+
+        $user->save();
+
+        return response()->json([
+            'statue' => 200,
+            'message' => 'user created'
+        ], 200);
     }
 
     public function login(Request $request) {
