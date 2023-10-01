a04cff2f621b7d9d8c0473ba3290354901e8e1fd
mdiktushar <md.tushar1804@gmail.com>
2023-10-01 14:08:28 +0600

    all courses

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -2,17 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class CourseController extends Controller
 {
     //
     public function courseEnrollment(Request $request) {
+        $input = request()->validate([
+            'title' => 'required',
+        ]);
 
+        auth()->user()->courses()->create($input);
+
+        return response()->json([
+            'status'=> 200,
+            'message' => 'Course Created'
+        ], 200);
     }
 
     public function totalCourses() {
+        $id = auth()->user()->id;
+        $courses = User::find($id)->courses;
 
+        return response()->json([
+            'status' => 200,
+            'message' => "All courses",
+            "courses" => $courses
+        ]);
     }
 
     public function deleteCourse() {
